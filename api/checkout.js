e69abe8c95b1917ef057e8657915d71a98147870
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-08 16:37:06 -0800

    pushing to gh

---
+++ api/checkout.js
@@ -0,0 +1,54 @@
+// const stripeAPI = require('../stripe');
+
+// async function createCheckoutSession(req, res) {
+//     const domainUrl = process.env.WEB_APP_URL;
+//     const { line_items, customer_email } = req.body;
+
+//     // check if req body has line items and email 
+//     // if there are no line items or customer email....
+//     if (!line_items || !customer_email) {
+//         return res.status(400).json({ error: 'missing required session parameters' });
+//     }
+
+//     let session;
+
+//     try {
+//         session = await stripeAPI.checkout.sessions.create({
+//             payment_method_types: ['card'],
+//             mode: 'payment',
+//             line_items,
+//             customer_email,
+//             success_url: `${domainUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
+//             cancel_url: `${domainUrl}/canceled`,
+//             shipping_address_collection: { allowed_countries: ['GB', 'US'] }
+//         });
+//         // res.redirect(303, session.url);
+//         res.status(200).json({ sessionId: session.id });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(400).json({ error: 'an error occured, unable to create session' });
+//     }
+// }
+
+// module.exports = createCheckoutSession;
+// This is your test secret API key.
+const stripe = require('stripe')(process.env.SECRET_KEY);
+const checkout = require('express').Router();
+
+
+checkout.post('/create-checkout-session', async (req, res) => {
+    console.log(req.body.line_items)
+    const line_items =req.body.line_items
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items,
+    mode: 'payment',
+    success_url: `${process.env.WEB_APP_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.WEB_APP_URL}/canceled`,
+    // automatic_tax: {enabled: true},
+  });
+  res.json({url:session.url, sessionId: session.id} );
+});
+
+module.exports =  checkout;
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config({ path: './.env' });
+const cors = require('cors');
+const createCheckoutSession = require('./api/checkout');
+const webhook = require('./api/webhook');
+const paymentIntent = require('./api/paymentintent');
+const decodeJWT = require('./auth/decodeJWT');
+const setupIntent = require('./api/setupIntent');
+const validateUser = require('./auth/validateUser');
+const getCards = require('./api/getPaymentMethod');
+const updatePaymentIntent = require('./api/updatePaymentIntent');
+
+const app = express();
+const port = 8080;
+
+app.use(express.json({
+    verify: (req, res, buffer) => req['rawBody'] = buffer,
+}));
+app.use(cors({ origin: true }));
+app.use(decodeJWT);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/get-payment-methods', validateUser, getCards);
+
+app.post('/create-checkout-session', createCheckoutSession);
+app.post('/webhook', webhook);
+app.post('/create-payment-intent', paymentIntent);
+app.post('/save-payment-method', validateUser, setupIntent);
+
+app.put('/update-payment-intent', validateUser, updatePaymentIntent);
+
+app.listen(port, () => console.log('server listening on port', port));
